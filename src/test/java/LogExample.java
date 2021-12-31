ac80be71e73a58121db40e3e45ef0d784fd8f378
SomeOverflow <jadenadedapo.mail@gmail.com>
2021-12-31 16:19:54 +0100

    Add Example

---
+++ src/test/java/LogExample.java
@@ -0,0 +1,44 @@
+import me.someoverflow.someutils.log.SomeLogFormatter;
+import me.someoverflow.someutils.log.SomeLogger;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * @author SomeOverflow
+ */
+public class LogExample {
+
+    public static void main(String[] args) {
+        SomeLogger logger = new SomeLogger("LogExample", true, "example/", "config.example");
+        logger.setFormatter(new SomeLogFormatter() {
+            @Override
+            public String format(String logger, SomeLogger.LogLevel logLevel, String message) {
+                SimpleDateFormat logTime = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+                Calendar cal = new GregorianCalendar();
+                cal.setTimeInMillis(System.currentTimeMillis());
+
+                return logTime.format(cal.getTime())
+                        + " - "
+                        + logLevel
+                        + " >> "
+                        + logger
+                        + " > "
+                        + message;
+            }
+        });
+
+        logger.logWF(SomeLogger.LogLevel.WARNING, "Warn Test", new SomeLogFormatter.Default());
+
+        logger.info("Info Test");
+
+        logger.setDebugging(true);
+        logger.debug("Debug Test");
+
+        logger.error("Error Test");
+
+        logger.log(SomeLogger.LogLevel.WARNING, "Another Warn Test");
+    }
+
+}
