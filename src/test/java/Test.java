ea6f1d56d946e4c1cb0455eca50337b436cabe1e
SomeOverflow <someoverflow@someworkflow.de>
2023-02-09 23:26:29 +0100

    Refactor
    Remove Test main

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -8,20 +8,6 @@ import java.io.IOException;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        SomeFile file = new SomeFile(System.getProperty("user.dir"), "/test.test");
-
-        try {
-            file.override("test1", "test2", "test3");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            file.write("Test", "Test", "Test");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public static void main(String[] args) { }
 
 }
